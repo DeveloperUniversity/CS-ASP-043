e6ca5e1b4a80056887b02a312da08c3d14e33703
Developer University <bob@devu.com>
2016-09-29 13:57:40 -0500

    Initial check in

---
+++ Lesson-43/Before/CS-ASP_043/CS-ASP_043/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS_ASP_043
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+ 
+
+        public string FormatDetailsForDisplay()
+        {
+            return String.Format("Make: {0} - Model: {1} - Year: {2} - Color: {3}", this.Make, this.Model, this.Year.ToString(), this.Color);
+        }
+
+
+    }
+}
